8301b306b14e875d9ed175b2438d6bb3c1f8af4a
Skylar Thomas <skylarthomas@skylars-mbp.home>
2019-04-09 21:56:33 -0700

    start serialize

---
+++ TiltWorld/Assets/Scripts/Serializer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Serializer : MonoBehaviour
+{
+
+    public PlayerController player;
+    public WorldController[] worlds;
+    public ObjectController[] objects;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
